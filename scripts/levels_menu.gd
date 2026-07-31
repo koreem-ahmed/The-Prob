065d8245e12b2e6159cbe52fbe07a38d5661d567
karim ahmed abd elkareem <karim.1024112@stemoctober.moe.edu.eg>
2026-07-31 03:24:46 +0300

    finishing the project and submitting

--- scripts/levels_menu.gd
+++ scripts/levels_menu.gd
@@ -1,10 +1,8 @@
 extends Control
 
-@onready var level_2_bocker: ColorRect = $"level 2 bocker"
-@onready var level_2_bocker_2: ColorRect = $"level 2 bocker2"
-@onready var level_3_bocker: ColorRect = $"level 3 bocker"
-@onready var level_3_bocker_2: ColorRect = $"level 3 bocker2"
-@onready var button_click: AudioStreamPlayer = $"button click"
+
+@onready var lock_lev_2: TextureRect = $lock_lev_2
+@onready var lock_lev_3: TextureRect = $lock_lev_3
 
 func _ready() -> void:
 	Global.score = 0
@@ -12,16 +10,12 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Global.win >= 1:
-		if is_instance_valid(level_2_bocker):
-			level_2_bocker.queue_free()
-		if is_instance_valid(level_2_bocker_2):
-			level_2_bocker_2.queue_free()
+		if is_instance_valid(lock_lev_2):
+			lock_lev_2.queue_free()
 	 
 	if Global.win >= 2:
-		if is_instance_valid(level_3_bocker):
-			level_3_bocker.queue_free()
-		if is_instance_valid(level_3_bocker_2):
-			level_3_bocker_2.queue_free()
+		if is_instance_valid(lock_lev_3):
+			lock_lev_3.queue_free()
 
 func _on_level_1_pressed() -> void:
 	TransitionLayer._transition()
